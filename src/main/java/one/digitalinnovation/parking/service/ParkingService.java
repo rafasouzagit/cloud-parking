4b48402958fc397adfa3b2e3c2dd97154c0dbc39
rafasouzagit <rafaelasouza.2811@gmail.com>
2022-08-11 23:19:31 -0300

    Adivionando Swagger

--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -4,6 +4,7 @@ import one.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.ApplicationScope;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -13,8 +14,11 @@ public class ParkingService {
     private static Map<String, Parking> parkingMap= new HashMap<>();
     static {
         var id = getUUID();
+        var id1 = getUUID();
         Parking parking = new Parking(id,"DMS-1111","SC", "CELTA","PRETO");
+        Parking parking1 = new Parking(id1,"WAS-1234","SP", "VW GOL","VERMELHO");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
 
     public List<Parking> findAll(){
@@ -22,5 +26,17 @@ public class ParkingService {
     }
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-","");
+    }
+
+    public Parking findById(String id) {
+        return  parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid,parkingCreate);
+        return parkingCreate;
     }
 }
